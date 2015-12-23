0defb99759a6b8db372cc069e736d2e41acbf636
Smitha Milli <smitha.milli@gmail.com>
2015-12-23 11:54:54 -0600

    Initial commit

---
+++ corenlp.py
@@ -0,0 +1,43 @@
+import json, requests
+
+class StanfordCoreNLP:
+
+    def __init__(self, server_url):
+        if server_url[-1] == '/':
+            server_url = server_url[:-1]
+        self.server_url = server_url
+
+    def annotate(self, text, properties=None):
+        if not properties:
+            properties = {}
+        r = requests.get(
+            self.server_url, params={
+                'properties': str(properties)
+            }, data=text)
+        output = r.text
+        if ('outputFormat' in properties 
+             and properties['outputFormat'] == 'json'):
+            try:
+                output = json.loads(output)
+            except:
+                pass
+        return output
+
+    def tokensregex(self, text, pattern, filter):
+        return self.regex('/tokensregex', text, pattern, filter)
+
+    def semgrex(self, text, pattern, filter):
+        return self.regex('/semgrex', text, pattern, filter)
+
+    def regex(self, endpoint, text, pattern, filter):
+        r = requests.get(
+            self.server_url + endpoint, params={
+                'pattern':  pattern,
+                'filter': filter
+            }, data=text)
+        output = r.text
+        try:
+            output = json.loads(r.text)
+        except:
+            pass
+        return output
