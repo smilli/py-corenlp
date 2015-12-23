b431ebedd25e05eadaec7d95131237713a382ef7
Smitha Milli <smitha.milli@gmail.com>
2015-12-23 12:18:59 -0600

    Add semgrex

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from corenlp import StanfordCoreNLP
 if __name__ == '__main__':
     nlp = StanfordCoreNLP('http://localhost:9000')
     text = (
-        'Pusheen and Smitha walked along the beach.  Pusheen wanted to surf,'
+        'Pusheen and Smitha walked along the beach. Pusheen wanted to surf,'
         'but fell off the surfboard.')
     output = nlp.annotate(text, properties={
         'annotators': 'tokenize,ssplit,pos,depparse,parse',
@@ -11,4 +11,6 @@ if __name__ == '__main__':
     })
     print(output['sentences'][0]['parse'])
     output = nlp.tokensregex(text, pattern='/Pusheen|Smitha/', filter=False)
+    print(output)
+    output = nlp.semgrex(text, pattern='{tag: VBD}', filter=False)
     print(output)
