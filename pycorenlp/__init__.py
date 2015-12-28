8cd8656d599499ea7fa25c68705a8f6010be11da
Smitha Milli <smitha.milli@gmail.com>
2015-12-28 00:01:08 -0600

    Add to PyPI

---
+++ pycorenlp/__init__.py
@@ -0,0 +1,1 @@
+from corenlp import StanfordCoreNLP
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pycorenlp',
+  packages = ['pycorenlp'],
+  version = '0.1',
+  description = 'Python wrapper for Stanford CoreNLP',
+  author = 'Smitha Milli',
+  author_email = 'smitha.milli@gmail.com',
+  url = 'https://github.com/smilli/py-corenlp',
+  download_url = 'https://github.com/smilli/py-corenlp/tarball/0.1',
+  keywords = ['nlp'],
+  classifiers = [],
+)
