cfae0b26896723c21116af109e9757990caf608c
Smitha Milli <smitha.milli@gmail.com>
2016-01-15 19:37:33 -0800

    PyPI update

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pycorenlp',
   packages = ['pycorenlp'],
-  version = '0.1.0',
+  version = '0.2.0',
   description = 'Python wrapper for Stanford CoreNLP',
   author = 'Smitha Milli',
   author_email = 'smitha.milli@gmail.com',
   url = 'https://github.com/smilli/py-corenlp',
-  download_url = 'https://github.com/smilli/py-corenlp/tarball/0.1',
+  download_url = 'https://github.com/smilli/py-corenlp/tarball/0.2',
   keywords = ['nlp'],
   classifiers = [],
   install_requires = [
